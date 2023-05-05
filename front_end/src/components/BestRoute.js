9099d5b9dc7f8fb8126dcb76ca972893656963cd
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-05-06 00:53:16 +0530

    best-routes

--- front_end/src/components/BestRoute.js
+++ front_end/src/components/BestRoute.js
@@ -92,6 +92,50 @@ export default function BestRoute() {
             </div>
           </div>
         </div>
+        <div className="best-flight-offer-card best-route-card">
+          <div className="best-flight-offer-card-image">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3QZG_ji_DnObJiNB1X91L2JBr5qs6ilcVeA&usqp=CAU"
+              alt="image"
+            />
+            <div className="best-route-detail-container">
+              <div className="best-route-detail">
+                <h3>Patna</h3>
+                <h4>
+                  DEL <i className="fa-solid fa-arrow-right"></i> PAT
+                </h4>
+              </div>
+              <div className="best-route-detail">
+                <h3>
+                  from <i className="fa-solid fa-indian-rupee-sign"></i>3524
+                </h3>
+                <h4>01 Aug 2023</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="best-flight-offer-card best-route-card">
+          <div className="best-flight-offer-card-image">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3QZG_ji_DnObJiNB1X91L2JBr5qs6ilcVeA&usqp=CAU"
+              alt="image"
+            />
+            <div className="best-route-detail-container">
+              <div className="best-route-detail">
+                <h3>Patna</h3>
+                <h4>
+                  DEL <i className="fa-solid fa-arrow-right"></i> PAT
+                </h4>
+              </div>
+              <div className="best-route-detail">
+                <h3>
+                  from <i className="fa-solid fa-indian-rupee-sign"></i>3524
+                </h3>
+                <h4>01 Aug 2023</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
